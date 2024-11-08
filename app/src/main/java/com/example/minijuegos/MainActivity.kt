69849ec45e619df048c607b0f88159f819747546
kennymen <kfloresc@ulasalle.edu.pe>
2024-11-08 13:37:27 -0500

    implementando primera interfaz del sistema para el muestreo de los minijuegos y selección

---
+++ app/src/main/java/com/example/minijuegos/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.minijuegos
+
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
